46a648971e8300a0e11808edf6762c5054db0f28
Anthony Young <404328+ayoungh@users.noreply.github.com>
2022-04-02 22:53:16 +0100

    Show Strava Activity

---
+++ apps/web/components/Activities/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import useSWR from 'swr';
+import { formatDistance } from 'date-fns';
+import fetcher from '@libs/fetcher';
+import { Text } from '@nextui-org/react';
+
+type activity = {
+  name: string;
+  start_date: string;
+  type: string;
+};
+
+export const Activities = () => {
+  const { data } = useSWR<activity[]>('/api/strava-activity', fetcher);
+
+  return (
+    <div>
+      <Text b>Last Activity:</Text>
+      <Text small>
+        {data &&
+          data.map(({ type, name, start_date }) => (
+            <div>
+              {type} - {name} -{' '}
+              {formatDistance(new Date(start_date), new Date(), {
+                addSuffix: true,
+              })}
+            </div>
+          ))}
+      </Text>
+    </div>
+  );
+};
