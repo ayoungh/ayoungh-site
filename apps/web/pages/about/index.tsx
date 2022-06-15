6237314536bfb99ebb31592e757bb10c2b385932
Anthony Young <404328+ayoungh@users.noreply.github.com>
2022-06-15 21:39:13 +0100

    Update the about page

--- apps/web/pages/about/index.tsx
+++ apps/web/pages/about/index.tsx
@@ -2,24 +2,35 @@ import { Text, Spacer, Container } from '@nextui-org/react';
 
 export default function About() {
   return (
-    <Container       css={{
+    <Container
+      css={{
         minHeight: 'calc(100vh - 140px)',
-      }}>
+      }}
+    >
       <Text
         h2
         size={25}
         css={{
-          textGradient: '45deg, $red500 50%, $blue500 30%',
+          textGradient: '45deg, $red500 0%, $blue500 30%',
         }}
         weight="bold"
       >
         About Me
       </Text>
       <Spacer y={3} />
-      <Text>Hey, I will be updating this section more soon but the down low:</Text>
-      <Text>I'm a Software Engineer working full time based in London.</Text>
-      <Text>I've been working in the web world for over 10 years. I started out doing graphic design and slowly edged into the coding side after building interactive projects in flash back in the day.</Text>
-      <Text>I then got into building tests (AB testing) on websites using jQuery and my world blew up. How much I could do using Javascript.</Text>
+      <Text>
+        I'm Currently working fulltime as a Senior Software Engineer and
+        Frontend Team Lead at Cado Security.
+      </Text>
+      <Text>I've been working in the web world for over 10 years. </Text>
+      <Text>
+        I started out doing graphic design and slowly edged into the coding side
+        after building interactive projects in flash back in the day.
+      </Text>
+      <Text>
+        I then got into building tests (AB testing) on websites using jQuery and
+        my world blew up. How much I could do using Javascript.
+      </Text>
       <Spacer y={3} />
     </Container>
   );
