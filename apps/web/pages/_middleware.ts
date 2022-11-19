b91aa9e4923cb5f107e602299cbe3d57d19a8cdb
Anthony Young <404328+ayoungh@users.noreply.github.com>
2022-11-19 22:34:32 +0000

    remove middleware test

--- apps/web/pages/_middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
-
-export async function middleware(req: NextRequest) {
-    console.log(req.referrer)
-//   return new Response('Testing');
-const response = NextResponse.next();
-return response;
-}
