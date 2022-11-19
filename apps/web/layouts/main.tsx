41fd9abfffd79cc7bb7bcbf46741e2f4e642b863
Anthony Young <404328+ayoungh@users.noreply.github.com>
2022-11-19 22:28:25 +0000

    do not show footer on blog pages

--- apps/web/layouts/main.tsx
+++ apps/web/layouts/main.tsx
@@ -4,14 +4,16 @@ import { Button } from '@nextui-org/react';
 import { Container, Row, Col } from '@nextui-org/react';
 import { Navigation } from '../components/Navigation';
 import { Footer } from '../components/Footer';
+import { useRouter } from 'next/router';
 
 export const MainLayout = ({ children }) => {
+  const router = useRouter();
+  console.log('router: ', router);
   return (
     <Container fluid>
       <Navigation />
       <Row>{children}</Row>
-
-      <Footer />
+      {!router.asPath.match(/blog/gi) && <Footer />}
     </Container>
   );
 };
