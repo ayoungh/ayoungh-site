8b45a15f2b076ed05ac196ee1080fe6335f8a1dd
Anthony Young <404328+ayoungh@users.noreply.github.com>
2022-11-25 11:05:37 +0000

    update config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,10 +2,6 @@ const withTM = require("next-transpile-modules")(["ui"]);
 
 module.exports = withTM({
   reactStrictMode: true,
-  // images: {
-  //   domains: ['cdn-images-1.medium.com'],
-    
-  // },
   images: {
     remotePatterns: [
       {
